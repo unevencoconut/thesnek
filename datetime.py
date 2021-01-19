1ed72568bd04c43a14c8884d88df15ebdf0007fc
Mark S <marktenchisilva@gmail.com>
2021-01-19 10:05:30 -0800

    Partially did the Dates and Matho Chapters. It appears my version of Python is too old, but at the same time, I could not upgrade through Virtual Environemtn. Strange. Need to troubleshoot. But for now, good stoppoing point

---
+++ datetime.py
@@ -0,0 +1,9 @@
+# Date Time Module ( built in )
+# Date is not a data type of its own, but datetime creates a data object to work with.
+# import datetime
+
+# x = datetime.timezone.utc
+
+# print(x)
+
+# Skipped - for some reason, I could not import Date Time Module
---
+++ math.py
@@ -0,0 +1,28 @@
+# Math
+# Python has built in Math Functions and a Math Module
+
+# Starting wit all the built in Python Functions
+
+# Find the Lowest or Highest Value in an Iterable
+x = min(5,10,25)
+y = max(5,10,25)
+
+print(x)
+print(y)
+
+# abs() returns the Absolute ( Postivie ) value of specific number
+x = abs(-7.25)
+print(x)
+
+# Power - returns the value of x to the power of y
+x = pow(4,3)
+print(x)
+
+
+# Now that you know all the built in functions
+# You can import the built in Math Module which extends Math Functions
+import math
+
+dir(math)
+# x = math.sqrt(64)
+# print(x)
